0b4aebcf76ba3d1c0aea423d8970928f3fe7193d
st0rmy <st0rmy@ya.ru>
2025-05-13 20:30:38 +0300

    1 коммит

---
+++ dz/1-converter/main.go
@@ -0,0 +1,13 @@
+package main
+
+// тарифы конвертации
+
+const USD_EUR float64 = 0.9
+const USD_RUB float64 = 80
+
+// EUR_RUB Согласно ТЗ Нужно расчитаит на основание первых двух пар
+const EUR_RUB float64 = 0
+
+func main() {
+
+}
